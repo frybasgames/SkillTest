42481f6e1a2205ab0104668765d9ed861976df28
frybasgames <frybasgames@gmail.com>
2021-02-23 09:42:21 +0300

    api.php change

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 
- Route::apiResource('school', 'App\Http\Controllers\SchoolController');
+Route::apiResource('school', 'App\Http\Controllers\SchoolController');
 
 Route::apiResource('campuses', 'App\Http\Controllers\CampusesController');
 
